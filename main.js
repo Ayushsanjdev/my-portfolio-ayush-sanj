5f26ab41bd060d8a5fa5f977f7198596d7c2047a
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-11 12:57:51 +0530

    added mobile nav button to all html file

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function directionNotification() {
-    alert("click on blue button and see accordingly!!, :) :D");
+// creating function for mobile navigation to work
+
+function toggleMobileNavigation() {
+    var element = document.getElementById("mobile-nav");
+
+    if (element.classList.contains("mobile-nav-open")) {
+
+        element.classList.remove("mobile-nav-open");
+    } else {
+        element.classList.add("mobile-nav-open");
+    }
 }
-var click = document.getElementById("direction").addEventListener("click", directionNotification());
+
+
+
+
+
+
+// function directionNotification() {
+//     alert("click on blue button and see accordingly!!, :) :D");
+// }
+// var click = document.getElementById("direction").addEventListener("click", directionNotification());

5ce27bddcccd0c411247f128a21165c58c84029d
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-11 10:52:35 +0530

    update: javascript added

---
+++ main.js
@@ -0,0 +1,4 @@
+function directionNotification() {
+    alert("click on blue button and see accordingly!!, :) :D");
+}
+var click = document.getElementById("direction").addEventListener("click", directionNotification());

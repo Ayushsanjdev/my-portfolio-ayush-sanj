373d838bcf818d2793de6701f0fc715fab0a5691
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-12 12:16:28 +0530

    updated: stack section

--- main.js
+++ main.js
@@ -10,8 +10,3 @@ function toggleMobileNavigation() {
         element.classList.add("mobile-nav-open");
     }
 }
-
-// function directionNotification() {
-//     alert("click on blue button and see accordingly!!, :) :D");
-// }
-// var click = document.getElementById("direction").addEventListener("click", directionNotification());
